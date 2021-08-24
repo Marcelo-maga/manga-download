60da77b1024e124bb1384551a4f9da6312085413
Marcelo Magalhães <marcelo.magalhaes13@etec.sp.gov.br>
2021-08-24 11:38:48 -0300

    Arrumando coisas

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-var mangaId = process.argv.slice(2)
+var ARGS = process.argv.slice(2)
 const functions = require('./functions')
 
 const puppeteer = require('puppeteer-extra')
@@ -11,7 +11,7 @@ const app = exprsss()
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
-app.get(`/caps/:id/`, async (req, res) => {
+app.get(`/main/:id/`, async (req, res) => {
   const id = req.params.id
 
   let mangaData = {
@@ -22,12 +22,12 @@ app.get(`/caps/:id/`, async (req, res) => {
   
   for(let i = 1; ; i++){
     var results = await functions.getCaps(id, i)
-
+  
     if (!mangaData.name) { 
       mangaData.id_serie = results.id_serie
       mangaData.name = results.name
     }
-
+  
     if (results.chapters.length > 0) {
       mangaData.chapters = mangaData.chapters.concat(results.chapters)
       continue
@@ -38,16 +38,16 @@ app.get(`/caps/:id/`, async (req, res) => {
 });
 
 app.listen(3000, () => {
-  console.log("Server is open")
+  console.log("Mangá Donwload by Marcelo-maga")
 })
 
+// Main function
 async function mangaDownload() {
 
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto(`http://localhost:3000/caps/${mangaId[0]}`);
+  await page.goto(`http://localhost:3000/main/${ ARGS[0] }`);
 
   await browser.close();
 }
-
 mangaDownload()
